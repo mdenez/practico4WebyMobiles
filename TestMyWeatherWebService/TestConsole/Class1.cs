abf5629b7d7ff9ebcdee21711e0b9233a32e8441
Martín Denez <martindenez@gmail.com>
2022-10-20 01:22:07 -0300

    [Agrego test por consola]

---
+++ TestMyWeatherWebService/TestConsole/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestConsole.WebService1;
+
+namespace TestConsole
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            WebService1SoapClient ws = new TestConsole.WebService1.WebService1SoapClient();
+            Double result = ws.getTemperature("Montevideo");
+            Console.WriteLine(String.Format("Temperatura de Montevideo {0} ", result));
+            Console.ReadLine();
+        }
+    }
+}
