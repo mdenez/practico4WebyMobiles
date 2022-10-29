95210559a680b9b0575c60fe61a13dcd90212eb9
Martín Denez <martindenez@gmail.com>
2022-10-29 01:34:04 -0300

    Se agrega proyecto unit test

---
+++ TestMyWeatherWebService/WeatherServiceUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeatherServiceUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeatherServiceUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c4f4670c-b725-44dc-a6c2-5b8c315fc26d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestMyWeatherWebService/WeatherServiceUnitTestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WeatherServiceUnitTestProject.ServiceReference1;
+
+namespace WeatherServiceUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            WebService1SoapClient ws = new ServiceReference1.WebService1SoapClient();
+            List<VOTemperature> lista = ws.ListTempreaturesByCity("Montevideo").ToList<VOTemperature>();
+            Assert.AreEqual(lista.Count, 5);
+
+        }
+    }
+}
